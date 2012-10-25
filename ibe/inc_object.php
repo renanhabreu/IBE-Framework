fc29608ab03750e23274092213a623b7afe4ca5a
Renan Abreu <renanhabreu@gmail.com>
2012-10-25 14:30:35 -0200

    new _construct method

--- ibe/inc_object.php
+++ ibe/inc_object.php
@@ -5,45 +5,66 @@
  *
  * @author Renan Abreu <renanhabreu@gmail.com>
  * @package ibe
- *
  */
 class Ibe_Object{
-
-
+	private $check_var_msg = 'Variavel dinamica nao foi encontrada no objeto';
+	private $check_var = FALSE;
     private $vars = array();
-
+	
+    public function __construct($check_var = FALSE,$msg = NULL){
+    	$this->check_var = $check_var;
+    	if(isset($msg)){
+    		$this->check_var_msg = $msg;
+    	}
+    }
+    
+    /**
+     * Inicializa as variaveis dinamicas de classe
+     * @param array $vars
+     * @return Ibe_Object
+     */
     public function __setVars($vars){
 
         if(sizeof($this->vars)){
            $this->vars =  array_merge($this->vars,$vars);
-        }else{
-            $this->vars = $vars;
         }
         return $this;
     }
-
+	
     public function __set($name, $value) {
         $this->vars[$name] = $value;
         return $this;
     }
 
+    /**
+     * Retorna a lista das variaveis dinamicas
+     * @return multitype:
+     */
     public function __getVars(){
         $vars = isset($this->vars)? $this->vars:array();
         return $vars;
     }
 
     public function __get($name) {
-        if(isset($this->vars[$name])){
-           return $this->vars[$name];
+    	
+        if($this->check_var && !isset($this->vars[$name])){
+        	throw new Ibe_Exception_Object($this->check_var_msg.' #VAR_NAME: $'.$name);
         }
-        return NULL;
+		
+        return $this->vars[$name];
     }
     
+    /**
+     * Inclui um arquivo php no contexto da classe
+     * @param string $filename
+     * @throws Ibe_Exception_Object
+     * @return string
+     */
     public function __include($filename){
         if(!file_exists($filename)){
         	$core = IBE_FRAMEWORK_PATH . 'default'. DS . $filename;
         	if(!file_exists($core)){
-            	throw new Ibe_Exception("Arquivo de objeto nao encontrado. [".$filename." ]");
+            	throw new Ibe_Exception_Object("Arquivo de objeto nao encontrado. [".$filename." ]");
         	}else{
         		$filename = $core;
         	}
@@ -75,16 +96,21 @@ class Ibe_Object{
      */
     public function isSetVar($name){
         if(!is_string($name)){
-            throw new Ibe_Exception(Ibe_Exception::TIPO_INVALIDO_DE_VARIAVEL,'string');
+            throw new Ibe_Exception_Object(Ibe_Exception::TIPO_INVALIDO_DE_VARIAVEL,'string');
         }
-        return isset ($this->vars[$name]);
+        return isset($this->vars[$name]);
     }
 
     public function toString(){
         return implode(',', $this->vars);
     }
-
+	
+    /**
+     * Transforma as variaveis dinamicas em 
+     * formato jSON
+     * @return string
+     */
     public function toJson(){
-        return json_encode($this->vars);
+        return json_encode($this->vars,JSON_FORCE_OBJECT);
     }
 }
